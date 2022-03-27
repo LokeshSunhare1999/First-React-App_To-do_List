66c6c0a0277bfa53bf123f7bf6a3e58c8464f64b
LokeshSunhare1999 <lokesh1999sunhare@gmail.com>
2022-03-27 22:37:08 +0530

    update About.js file path to MyComponent

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
-
 import './App.css';
-import Header from './MyComponent/Header';
-import { Todos } from './MyComponent/Todos';
-import { Footer } from './MyComponent/Footer';
-import { AddTodo } from './MyComponent/AddTodo';
-import { About } from '. /MyComponent/About';
+import Header from "./MyComponent/Header";
+import { Todos } from "./MyComponent/Todos";
+import { Footer } from "./MyComponent/Footer";
+import { AddTodo } from "./MyComponent/AddTodo";
+import { About } from "./MyComponent/About";
 import React, { useState, useEffect } from 'react';
-
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  
+  Route
 } from "react-router-dom";
 
-
 function App() {
-
   let initTodo;
   if (localStorage.getItem("todos") === null) {
     initTodo = [];
@@ -25,27 +20,29 @@ function App() {
     initTodo = JSON.parse(localStorage.getItem("todos"));
   }
 
+
   const onDelete = (todo) => {
-    console.log("I am on Delete of todo", todo);
-    // deleteing this way in react does not work
+    console.log("I am ondelete of todo", todo);
+    // Deleting this way in react does not work
     // let index = todos.indexOf(todo);
-    // todos.splice(index,1);
+    // todos.splice(index, 1);
+
     setTodos(todos.filter((e) => {
       return e !== todo;
     }));
+    console.log("deleted", todos)
     localStorage.setItem("todos", JSON.stringify(todos));
   }
 
   const addTodo = (title, desc) => {
     console.log("I am adding this todo", title, desc)
     let sno;
-    if (todos.length == 0) {
+    if (todos.length === 0) {
       sno = 0;
     }
     else {
       sno = todos[todos.length - 1].sno + 1;
     }
-
     const myTodo = {
       sno: sno,
       title: title,
@@ -56,7 +53,6 @@ function App() {
   }
 
   const [todos, setTodos] = useState(initTodo);
-
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos])
@@ -67,13 +63,12 @@ function App() {
         <Header title="My Todos List" searchBar={false} />
         <Switch>
           <Route exact path="/" render={() => {
-            return(
-            <>
-              <AddTodo addTodo={addTodo} />
-              <Todos todos={todos} onDelete={onDelete} />
-            </>)
+            return (
+              <>
+                <AddTodo addTodo={addTodo} />
+                <Todos todos={todos} onDelete={onDelete} />
+              </>)
           }}>
-          
           </Route>
           <Route exact path="/about">
             <About />
